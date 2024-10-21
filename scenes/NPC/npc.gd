d6f37f9fd75e38951365477ea418705478e3066e
nhan.trinh <hongnhan50@gmail.com>
2024-10-22 06:50:08 +0700

    Implement NPC

--- scenes/NPC/npc.gd
+++ scenes/NPC/npc.gd
@@ -4,9 +4,9 @@ extends CharacterBody2D
 @onready var label: Label = $Label
 @onready var nav_agent: NavigationAgent2D = $NavAgent
 
-const SPEED: float = 60
+const SPEED: float = 120
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("set_target"):
 		nav_agent.target_position = get_global_mouse_position()
 	
@@ -16,10 +16,10 @@ func _physics_process(delta: float) -> void:
 
 func set_label():
 	var debug_msg = ""
-	debug_msg += "Done: \n" + str(nav_agent.is_navigation_finished())
-	debug_msg += "Reachable: \n" + str(nav_agent.is_target_reachable())
-	debug_msg += "Reached: \n" + str(nav_agent.is_target_reached())
-	debug_msg += "Target: \n" + str(nav_agent.target_position)
+	debug_msg += "Done: " + str(nav_agent.is_navigation_finished()) + "\n"
+	debug_msg += "Reachable: " + str(nav_agent.is_target_reachable()) + "\n"
+	debug_msg += "Reached: " + str(nav_agent.is_target_reached()) + "\n"
+	debug_msg += "Target: " + str(nav_agent.target_position) + "\n"
 	
 	label.text = debug_msg
 
