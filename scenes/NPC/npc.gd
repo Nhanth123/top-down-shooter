24c760b998fcddd2539fc35990b9f8068a7bd7c4
nhan.trinh <hongnhan50@gmail.com>
2024-11-03 01:59:48 +0700

    Feature: implement player movement

--- scenes/NPC/npc.gd
+++ scenes/NPC/npc.gd
@@ -4,14 +4,44 @@ extends CharacterBody2D
 @onready var label: Label = $Label
 @onready var nav_agent: NavigationAgent2D = $NavAgent
 
+@export var patrol_points: NodePath
+
+var _waypoints: Array = []
+var _current_wp: int  = 0
+
+
 const SPEED: float = 120
+
+
+func _ready() -> void:
+	set_physics_process(false)
+	create_waypoint()
+	call_deferred("set_physics_process", true)
+	
+
+func create_waypoint():
+	for c in get_node(patrol_points).get_children():
+		_waypoints.append(c.global_position)
+
 
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("set_target"):
 		nav_agent.target_position = get_global_mouse_position()
 	
 	update_navigation()
+	process_patrolling()
 	set_label()
+
+func process_patrolling() -> void:
+	if nav_agent.is_navigation_finished() == true:
+		navigate_wp()
+	
+
+func navigate_wp() -> void:
+	if _current_wp >= len(_waypoints):
+		_current_wp = 0
+	nav_agent.target_position = _waypoints[_current_wp]
+	_current_wp += 1
 
 
 func set_label():
---
+++ scenes/Player/player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+const SPEED: float = 180
+
+
+
+
+func _ready() -> void:
+	pass
+	
+func _physics_process(delta: float) -> void:
+	get_input()
+	move_and_slide()
+	rotation = velocity.angle()
+
+func get_input():
+	var nv = Vector2.ZERO
+	nv.x = Input.get_action_strength("right") - Input.get_action_strength("left") 
+	nv.y = Input.get_action_strength("down") - Input.get_action_strength("up") 
+	velocity = nv * SPEED
