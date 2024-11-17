2743cb8da9f80f59edafd5aac112e5e0100414a7
nhan.trinh <hongnhan50@gmail.com>
2024-11-17 21:58:29 +0700

    Feature: detect the exit of pill on scene

---
+++ scenes/level_map.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+@onready var pickups: Node = $Pickups
+
+
+var _pickups_count: int = 0
+var _collected: int = 0
+
+func _ready() -> void:
+	_pickups_count = pickups.get_children().size()
+	SignalManager.on_pickup.connect(_on_pickup)
+
+func check_show_exit():
+	if _collected == _pickups_count:
+		SignalManager.on_show_exit.emit()
+		print("on_show_exit")
+
+
+func _on_pickup() -> void:
+	_collected += 1
+	check_show_exit()
