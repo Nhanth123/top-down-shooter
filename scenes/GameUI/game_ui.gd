6fc14c22a3924f6e5d1afe8140be1d2a6277f268
nhan.trinh <hongnhan50@gmail.com>
2024-11-24 01:23:02 +0700

    Feature: improve game UI

---
+++ scenes/GameUI/game_ui.gd
@@ -0,0 +1,25 @@
+extends Control
+
+@onready var score_label: Label = $MarginContainer/ScoreLabel
+@onready var exit_label: Label = $MarginContainer/ExitLabel
+@onready var time_label: Label = $MarginContainer/TimeLabel
+@onready var game_over: Label = $ColorRect/GameOver
+@onready var color_rect: ColorRect = $ColorRect
+
+var _time: float = 0.0
+
+func _ready() -> void:
+	SignalManager.on_show_exit.connect(_on_show_exit)
+	SignalManager.on_exit.connect(_on_exit)
+
+func _process(delta: float) -> void:
+	_time += delta
+	time_label.text = "%.1fs" % _time
+	
+func _on_show_exit():
+	exit_label.show()
+
+func _on_exit():
+	set_process(false)
+	color_rect.show()
+	game_over.text = "Well done, you took: %.1f seconds" % _time
